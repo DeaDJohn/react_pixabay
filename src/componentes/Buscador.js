2d91ddab45ac75b08327b24b7af9de2dbabddb28
Juanjo <juanjofb86@gmail.com>
2019-08-29 21:11:15 +0200

    Continuar

---
+++ src/componentes/Buscador.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+
+import Error from './Error';
+
+const Buscador = () => {
+
+    const [terminoBusqueda, guardarTerminoBusqueda] = useState();
+    const [error, guardarError] = useState(false);
+
+    const buscarImagen = e => {
+        e.preventDefault();
+
+        // validar
+        if(terminoBusqueda === ''){
+            guardarError(true);
+            return;
+        }
+        // enviar el termino al componente principal
+        guardarError(false);
+        console.log(error);
+    } 
+
+    return ( 
+        <form
+            onSubmit={buscarImagen}
+        >
+            <div className="row">
+                <div className="form-group col-md-8">
+                    <input 
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="Busca una imagen: Por ejemplo 'Futbol'"
+                        onChange= { e => guardarTerminoBusqueda(e.target.value) }
+                    />
+                </div>
+                <div className="form-group col-md-4">
+                    <input 
+                        type="submit"
+                        className="btn btn-lg btn-danger btn-block"
+                        value="Buscar"
+                    />
+                </div>
+            </div>
+            { (error)? <Error mensaje="Agrega un termino de busqueda" /> : null  }
+        </form>
+     );
+}
+ 
+export default Buscador;
